81c64489b37d319d319b7848a51935933de26062
Sourabh Kushwah <83066893+sourabhk2003@users.noreply.github.com>
2025-05-24 13:20:21 +0530

    Update web-app.py

--- web-app.py
+++ web-app.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Aug 28 00:24:01 2024
 
-@author: shrut
+@author: sourk2003(sourabh)
 """
 
 import numpy as np
@@ -11,7 +11,7 @@ import streamlit as st
 import sklearn 
 
 
-loaded_model = pickle.load(open('C:/Users/shrut/breast cancer/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('/content/trained_model.sav', 'rb'))
 
 input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
 
@@ -92,8 +92,3 @@ def main():
 if __name__ == '__main__':
     main()
     
-    
-    
-    
-    
- 
